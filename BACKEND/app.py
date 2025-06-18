9b7497a5dd640d803e23ce83f603857a62b2f719
Emmanuel Otieno <otienoemmanuel717@gmail.com>
2025-06-18 11:03:53 +0300

    first commit

---
+++ BACKEND/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.auth import auth_bp
+
+app = Flask(__name__)
+CORS(app)  # Allow frontend to talk to backend
+
+app.register_blueprint(auth_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
